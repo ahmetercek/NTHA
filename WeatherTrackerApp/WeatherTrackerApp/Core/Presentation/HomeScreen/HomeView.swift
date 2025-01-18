261014295d6bdb53177693f0fdc219275b5daa47
“ahmetercek” <ercek.ahmett@gmail.com>
2025-01-18 14:57:31 -0500

    TASK: Project folder structure arranged

---
+++ WeatherTrackerApp/WeatherTrackerApp/Core/Presentation/HomeScreen/HomeView.swift
@@ -0,0 +1,17 @@
+//
+//  HomeView.swift
+//  WeatherTrackerApp
+//
+//  Created by Ahmet on 18.01.2025.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    
+    @StateObject private var viewModel = HomeViewModel()
+    
+    var body: some View {
+        Text("Hello World!")
+    }
+}
---
+++ WeatherTrackerApp/WeatherTrackerApp/Core/Presentation/HomeScreen/HomeViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewModel.swift
+//  WeatherTrackerApp
+//
+//  Created by Ahmet on 18.01.2025.
+//
+
+import Foundation
+
+final class HomeViewModel: ObservableObject {
+    
+    
+    
+    init() {
+        
+    }
+}
