7f69d4ba88f62ccd45ace05be130ec8dad8a8598
“ahmetercek” <ercek.ahmett@gmail.com>
2025-01-18 14:44:21 -0500

    init project

---
+++ WeatherTrackerApp/WeatherTrackerApp/WeatherTrackerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherTrackerAppApp.swift
+//  WeatherTrackerApp
+//
+//  Created by Ahmet on 18.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct WeatherTrackerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
